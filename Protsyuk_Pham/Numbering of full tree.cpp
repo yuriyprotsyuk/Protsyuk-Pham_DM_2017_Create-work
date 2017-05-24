b2eda357ccb192f2a85fa44a07c6924146bdd55d
yuriyprotsyuk <yuriy.protsyuk1999@gmail.com>
2017-05-24 22:37:47 +0300

    Third commit

--- Protsyuk_Pham/Numbering of full tree.cpp
+++ Protsyuk_Pham/Numbering of full tree.cpp
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <math.h>
 void calc();
+int n, m;
+int f[25][25];
+int vertex[25][25];
 int min(int a, int b)
 {
 	if (a < b) return a;
@@ -14,8 +17,29 @@ int max(int a, int b)
 	if (a < b) return b;
 	else return a;
 }
-int n, m;
-int f[25][25];  //не поміститься, потрібен структурований тип
+int c(int n, int m)
+{
+	int x = 1;
+	m = min(m, n - m);
+	for (int i = 0; i < m; i++) {
+		int t = (n - i);
+		x = x * t / (i + 1);
+	}
+	return x;
+}
+void calc()
+{	
+	for (int i = 1; i <= 21; i++) {
+		f[i][0] = 1; vertex[i][0] = 1;
+		for (int j = 1; j <= 21 / i; j++) {
+			f[i][j] = 1;
+			for (int k = 0; k < i; k++) {
+				vertex[i][j] = vertex[i][j - 1] * i + 1;
+				f[i][j] = f[i][j] * c(vertex[i][j] - 1 - k * vertex[i][j - 1], vertex[i][j - 1]) * f[i][j - 1];
+			}
+		}
+	}
+}
 int main()
 {
 	calc();
