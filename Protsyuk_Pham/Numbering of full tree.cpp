c9c00bde524088cc7deb7c83e119a068ef20535c
yuriyprotsyuk <yuriy.protsyuk1999@gmail.com>
2017-05-25 11:55:57 +0300

    Fourth commit

--- Protsyuk_Pham/Numbering of full tree.cpp
+++ Protsyuk_Pham/Numbering of full tree.cpp
@@ -3,9 +3,41 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+const int MAXSIZE = 10000;
+struct add
+{
+	int s[MAXSIZE];
+	add() { memset(s, 0, sizeof(s)); }
+	add(int number) { *this = number; }
+	add(const char* number) { *this = number; }
+	void put();
+	add mul(int d);
+	void del();
+	void init() { memset(s, 0, sizeof(s)); }
+
+	add operator =  (char *num);
+	add operator =  (int num);
+
+	bool operator <  (const add& b) const;
+	bool operator >  (const add& b) const { return b < *this; }
+	bool operator <= (const add& b) const { return !(b < *this); }
+	bool operator >= (const add& b) const { return !(*this < b); }
+	bool operator != (const add& b) const { return b < *this || *this < b; }
+	bool operator == (const add& b) const { return !(b != *this); }
+
+	add operator + (const add& c);
+	add operator * (const add& c);
+	add operator - (const add& c);
+	int  operator / (const add& c);
+	add operator / (int k);
+	add operator % (const add &c);
+	int  operator % (int k);
+	void operator ++ ();
+	bool operator -- ();
+};
 void calc();
 int n, m;
-int f[25][25];
+add f[25][25];
 int vertex[25][25];
 int min(int a, int b)
 {
@@ -17,12 +49,12 @@ int max(int a, int b)
 	if (a < b) return b;
 	else return a;
 }
-int c(int n, int m)
+add c(int n, int m)
 {
-	int x = 1;
+	add x = 1;
 	m = min(m, n - m);
 	for (int i = 0; i < m; i++) {
-		int t = (n - i);
+		add t = (n - i);
 		x = x * t / (i + 1);
 	}
 	return x;
